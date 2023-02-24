948f98739a4d32d40f97c73daee66e5fb8656d98
Alex Rice <rice.alexanderb@gmail.com>
2023-02-23 18:41:26 -0600

    Fix header insertion

--- Scripts/copyright.rb
+++ Scripts/copyright.rb
@@ -10,8 +10,8 @@ exclusions = %w[Scripts .github]
 # Find all resources in the Chartboost Mediation repo that match the accepted file extensions
 files = []
 
-# We expect to be in the Scripts folder so go one level up to the root of the repo
-Find.find("..") do |path|
+# Script will be run from the root of the repo
+Find.find(".") do |path|
   files << path if file_extensions_accepted.include? File.extname(path)
 end
 
@@ -31,23 +31,31 @@ end
 copyright_notice = " Copyright 2022-#{Time.now.year} Chartboost, Inc.\n\n Use of this source code is governed by an MIT-style\n license that can be found in the LICENSE file."
 files_have_been_changed = false
 
-files.each do |file|
+files.each do |file_path|
   # Dynamically add comment formatting to header so we can adjust for different file types in the future
   notice_as_comment = "//" + copyright_notice.gsub(/\n/, "\n//") + "\n"
-  File.open(file, 'r+') do |f|
+  File.open(file_path, 'r') do |f|
     contents = f.read
     if contents.start_with?(notice_as_comment)
       next
     else
-      f.rewind
-      # If the file starts with a comment block, replace it with the copyright header
-      if contents.start_with?("//")
-        f.puts contents.gsub(/^(?:\/\/[^\n]*\n)+/, notice_as_comment)
-      # If there's no comment at the top just insert the copyright header
-      else
-        f.puts notice_as_comment + contents
+      tmp = Tempfile.new("tmp")
+      begin
+        if contents.start_with?("//")
+          # If the file starts with a comment block, replace it with the copyright header
+          tmp.puts contents.sub(/^(?:\/\/[^\n]*\n)+/, notice_as_comment)
+        else
+          # If there's no comment at the top just insert the copyright header
+          tmp.puts notice_as_comment + contents
+        end
+
+        # Replace the original file with the new one
+        FileUtils.mv(tmp.path, file_path)
+        files_have_been_changed = true
+      ensure
+        tmp.close
+        tmp.unlink
       end
-      files_have_been_changed = true
     end
   end
 end
