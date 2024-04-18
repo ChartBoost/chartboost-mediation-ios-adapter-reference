20b52762aa1c7a6ca64bbb0e05952ffbbba32c3d
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 10:02:36 +0530

    Update show method

--- Source/ReferenceAdapterBannerAd.swift
+++ Source/ReferenceAdapterBannerAd.swift
@@ -51,14 +51,6 @@ final class ReferenceAdapterBannerAd: ReferenceAdapterAd, PartnerAd {
         // For simplicity, the current implementation always assumes successes.
         completion(.success([:]))
     }
-
-    /// Shows a loaded ad.
-    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
-    /// - parameter viewController: The view controller on which the ad will be presented on.
-    /// - parameter completion: Closure to be performed once the ad has been shown.
-    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerDetails, Error>) -> Void) {
-        // no-op
-    }
     
     /// Map Chartboost Mediation's banner sizes to the Reference SDK's supported sizes.
     /// - Parameter size: The Chartboost Mediation's banner size.
